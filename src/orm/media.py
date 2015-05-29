88632c301bdb728732cb84d75863b75552426f97
Omer Ben-Amram <omerbenamram@gmail.com>
2015-05-29 18:38:50 +0300

    updated orm, added simple db for testing

--- src/orm/media.py
+++ src/orm/media.py
@@ -4,31 +4,71 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
 Base = declarative_base()
 
-class Album(Base):
-    __tablename__ = 'albums'
+"""
+Many to one relation in ascending order.
+Artist --> Albums --> Releases --> Tracks
+"""
 
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    asin = Column(String)
-    release_date = Column(DateTime)
-    musicbrainz_id = Column(String)
-    musicbrainz_release_group_id = Column(String, nullable=False)
-    artist_id = Column(Integer, ForeignKey('artists.id'))
-
-    artist = relationship("Artist", backref=backref('albums', order_by=id))
-
-    def __repr__(self):
-        return "<Album {name}, released in {date}, release_group_id {id}>".format(name=self.title,
-                                                                                  date=self.release_date,
-                                                                                  id=self.musicbrainz_release_group_id)
 
 class Artist(Base):
     __tablename__ = 'artists'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    musicbrainz_id = Column(String)
+    musicbrainz_id = Column(String, unique=True, nullable=False)
 
     def __repr__(self):
         return "<Artist {name}, musicbrainz_id {id}>".format(name=self.name,
                                                              id=self.musicbrainz_id)
+
+
+class Album(Base):
+    __tablename__ = 'albums'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    musicbrainz_id = Column(String, unique=True, nullable=False)
+
+    artist_id = Column(Integer, ForeignKey('artists.id'))
+    artist = relationship("Artist", backref=backref('albums', order_by=id))
+
+    def __repr__(self):
+        return "<Album {name}, id {id}>".format(name=self.title,
+                                                date=self.musicbrainz_id)
+
+
+class Release(Base):
+    __tablename__ = 'releases'
+
+    id = Column(Integer, primary_key=True)
+    release_date = Column(DateTime)
+    name = Column(String)
+    musicbrainz_release_group_id = Column(String, unique=True, nullable=False)
+    number_of_tracks = Column(Integer)
+
+    album_id = Column(Integer, ForeignKey('albums.id'))
+    album = relationship("Release", backref=backref('releases', order_by=id))
+
+    def __repr__(self):
+        return "<Album {album}, release_id {id}>".format(album=self.album,
+                                                         id=self.id)
+
+
+class Track(Base):
+    __tablename__ = 'tracks'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    title = Column(String)
+    number = Column(Integer)
+    musicbrainz_id = Column(String, unique=True)
+    duration = Column(Integer)
+    bitrate = Column(Integer)
+
+    release_id = Column(Integer, ForeignKey('releases.id'))
+    release = relationship("Track", backref=backref('tracks', order_by=id))
+
+    def __repr__(self):
+        return "<Track {releasename}/{number}-{name}>".format(releasename=self.release.name,
+                                                              number=self.number,
+                                                              name=self.name)

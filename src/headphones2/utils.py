5df10de72f393eb09f9f575f0eb2906d7a14b4fa
Omer Ben-Amram <omerbenamram@gmail.com>
2015-05-30 14:18:02 +0300

    added utility functions

--- src/headphones2/utils.py
+++ src/headphones2/utils.py
@@ -11,3 +11,18 @@ def find_artist_by_name(name, limit=10, wanted_keys=('name', 'id', 'country', 'e
     # return a list of dicts containing only the wanted values
     return [{k: v for k, v in d.items() if k in wanted_keys} for d in sorted_by_score]
 
+# TODO: search by releasegrouop ID
+def find_releases(name, limit=10, artist_id=None,
+                  wanted_keys=('name', 'id', 'title', 'country', 'release-group', 'ext:score', 'asin')):
+    params = {'release': name, 'arid': artist_id}
+    search_results = musicbrainzngs.search_releases(limit=limit, **params)['release-list']
+    sorted_by_score = sorted(search_results, cmp=lambda x, y: max(x, y), key=lambda d: int(d['ext:score']))
+    return [{k: v for k, v in d.items() if k in wanted_keys} for d in sorted_by_score]
+
+# TODO: return better dict
+def find_albums(name, limit=10, artist_id=None,
+                wanted_keys=('name', 'id', 'title', 'country', 'release-group', 'ext:score', 'asin', 'artist-credit')):
+    params = {'releasegroup': name, 'arid': artist_id}
+    search_results = musicbrainzngs.search_release_groups(limit=limit, strict=True, **params)['release-group-list']
+    sorted_by_score = sorted(search_results, cmp=lambda x, y: max(x, y), key=lambda d: int(d['ext:score']))
+    return [{k: v for k, v in d.items() if k in wanted_keys} for d in sorted_by_score]

f45d9ff904c72bb582c1862072121741e28018f0
Omer Ben-Amram <omerbenamram@gmail.com>
2015-05-31 15:53:18 +0300

    added a simple web test

--- test/conftest.py
+++ test/conftest.py
@@ -3,3 +3,5 @@ import vcr as _vcr
 
 CASSETTE_LIBRARY_DIR = os.path.join(__file__, '..', 'fixtures', 'cassettes')
 vcr = _vcr.VCR(cassette_library_dir=CASSETTE_LIBRARY_DIR)
+
+
---
+++ test/test_web_pages.py
@@ -0,0 +1,16 @@
+import pytest
+
+from flask import Response
+from headphones2.app import app as _app
+
+
+@pytest.fixture
+def app():
+    app = _app
+    app.response_class = Response
+    return app
+
+def test_load_artist_page(client):
+    artist_id = '7bbfd77c-1102-4831-9ba8-246fb67460b3'  # Ayreon.
+    res = client.get('/artistPage?ArtistID={}'.format(artist_id))
+    assert res.status_code == 200

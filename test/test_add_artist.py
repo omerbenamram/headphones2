f1e02e091bbfab749aabe4389b72e34e5a74cc94
Omer Ben-Amram <omerbenamram@gmail.com>
2015-06-03 13:46:45 +0300

    added test

--- test/test_add_artist.py
+++ test/test_add_artist.py
@@ -12,7 +12,7 @@ musicbrainzngs.set_rate_limit(False)
 
 @pytest.yield_fixture()
 def session(tmpdir):
-    db_file = DB_FILE #os.path.join(tmpdir.strpath, 'temp.db')
+    db_file = os.path.join(tmpdir.strpath, 'temp.db')
     if os.path.exists(db_file):
         os.remove(db_file)
 
@@ -44,3 +44,36 @@ def test_add_artist_to_db(session):
     release = releases.filter_by(asin='B00F2HW220').first()
     assert release.tracks.count() == 42
     assert release.release_date == datetime.datetime(2013, 10, 25)
+
+@vcr.use_cassette()
+def test_delete_artist_from_db(session):
+    # add artist test case
+    artist_id = '7bbfd77c-1102-4831-9ba8-246fb67460b3'  # Ayreon.
+
+    add_artist_to_db(artist_id, session)
+
+    artist = session.query(Artist).filter_by(musicbrainz_id=artist_id).first()
+
+    assert artist
+    assert artist.name == 'Ayreon'
+    assert artist.musicbrainz_id == artist_id
+
+    toe_album = artist.albums.filter_by(title='The Theory of Everything').first()
+    assert toe_album
+    assert toe_album.musicbrainz_id == '6281bcfe-058e-4cd3-85bc-66f47c28960b'
+
+    releases = session.query(Release).filter_by(album=toe_album)
+
+    assert releases.count() == 5
+    release = releases.filter_by(asin='B00F2HW220').first()
+    assert release.tracks.count() == 42
+    assert release.release_date == datetime.datetime(2013, 10, 25)
+
+    session.delete(artist)
+    session.commit()
+
+    artist = session.query(Artist).filter_by(musicbrainz_id=artist_id).first()
+
+    assert not artist
+    albums = session.query(Album).filter_by(artist=artist)
+    assert albums.count() == 0

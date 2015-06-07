85ba4067f708e291fc1667b0f76261a1c23cab99
Omer Ben-Amram <omerbenamram@gmail.com>
2015-06-08 00:59:02 +0300

    modified simpletagger

--- src/headphones2/media/simple_tagger.py
+++ src/headphones2/media/simple_tagger.py
@@ -1,23 +1,25 @@
+import logbook
 from beets.autotag import tag_album, Recommendation
 from headphones2.media.tagger import Tagger
 
+logger = logbook.Logger(__name__)
 
 class SimpleBeetsTagger(Tagger):
-    def __init__(self, task, expected_artist=None, expected_album=None):
-        super(SimpleBeetsTagger).__init__(task)
-        self._expected_artist = expected_artist
-        self._expected_album = expected_album
-        self._results = []
 
-    @property
-    def results(self):
-        if not self._results:
-            self.tag()
-        return self._results
+    modifies = []
+    kind = 'MetadataProcessor'
 
-    def process(self):
+    def __init__(self):
+        super(SimpleBeetsTagger).__init__()
+
+    # TODO: implement metadata editing
+    @staticmethod
+    def process(list_of_items, expected_artist=None, expected_album=None):
         artist_name, album_name, album_recommendation_list, recommendation = \
-            tag_album(self._task.items, search_artist=self._expected_artist, search_album=self._expected_album)
+            tag_album(list_of_items, search_artist=expected_artist, search_album=expected_album)
 
-        if recommendation is not Recommendation.none:
-            self._results.append(album_recommendation_list[0].album_id)
+        if recommendation is Recommendation.none:
+            logger.debug('Failed matching album')
+            return None
+        else:
+            return album_recommendation_list[0].album_id

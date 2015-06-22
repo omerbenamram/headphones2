ecc2856de76fb123c7ecf353edfea7b74b3030b2
Omer Ben-Amram <omerbenamram@gmail.com>
2015-06-22 23:01:04 +0300

    acoustid is now invoked from inside beets tagger

--- src/headphones2/postprocess/process_folder.py
+++ src/headphones2/postprocess/process_folder.py
@@ -7,8 +7,7 @@ import py
 
 from beets.library import Item
 from headphones2.config import MEDIA_FORMATS
-from headphones2.postprocess import AcoustIDAlbumTagger, BeetsTagger, PostProcessorException, Renamer
-from headphones2.postprocess.taggers.simple_tagger import BeetsTaggerException
+from headphones2.postprocess import AcoustIDAlbumTagger, BeetsTagger, Renamer
 
 logger = logbook.Logger(__name__)
 Path = py.path.local
@@ -31,6 +30,7 @@ def _collect_files_from_folder(folder_path, wanted_formats=MEDIA_FORMATS):
 
 def post_process_folder(folder, expected_artist=None, expected_album=None, should_move=False,
                         flatten_result_folder=False):
+
     logger.info("Started post processing for {}".format(folder))
     logger.debug("Collecting media items from folders")
 
@@ -45,22 +45,14 @@ def post_process_folder(folder, expected_artist=None, expected_album=None, shoul
     for tagger in [aid_tagger, beets_tagger, renamer]:
         post_processors.append(tagger)
 
-    try:
-        beets_result = beets_tagger.process(items, expected_artist=expected_artist, expected_album=expected_album)
-    except BeetsTaggerException:  # beets tagging flow failed
-        logger.exception('Calling AcoustID Tagger')
-        recommendation = aid_tagger.process(items)
-        if recommendation:
-            beets_result = beets_tagger.process(items, expected_artist=expected_artist,
-                                                expected_album=expected_album,
-                                                expected_release_id=recommendation)
-        else:
-            raise PostProcessorException("Exhasted tagging options, failing")
+    beets_result = beets_tagger.process(items, expected_artist=expected_artist, expected_album=expected_album,
+                                        fallback_taggers=[aid_tagger])
 
     renamer.process(items, flatten_folder=flatten_result_folder)
 
     _write_all_changes(items, list_of_post_processors=post_processors,
-                       album_info=beets_result.album_info_object, track_mapping=beets_result.track_mapping_object)
+                       album_info=beets_result.album_info_object, track_mapping=beets_result.track_mapping_object,
+                       should_move=should_move)
 
     logger.info("Post processor compelted for {}".format(folder))
 
--- src/headphones2/postprocess/taggers/simple_tagger.py
+++ src/headphones2/postprocess/taggers/simple_tagger.py
@@ -9,21 +9,31 @@ from headphones2.postprocess.component_base import PostProcessor, PostProcessorE
 
 logger = logbook.Logger(__name__)
 
-BeetsTaggerResult = namedtuple("Result", ["album_id", "album_info_object", "track_mapping_object", "extra_items", "extra_tracks"])
+BeetsTaggerResult = namedtuple("Result",
+                               ["album_id", "album_info_object", "track_mapping_object", "extra_items", "extra_tracks"])
+
 
 class BeetsTagger(PostProcessor):
     def __init__(self):
         super(BeetsTagger, self).__init__()
 
     def process(self, list_of_items, expected_artist=None, expected_album=None, expected_release_id=None,
-                fallback_tagger=None):
+                fallback_taggers=()):
         artist_name, album_name, album_recommendation_list, recommendation = \
             tag_album(list_of_items, search_artist=expected_artist, search_album=expected_album,
                       search_id=expected_release_id)
 
         if recommendation is Recommendation.none:
             logger.debug('{} Failed to match album'.format(__name__))
-            raise BeetsTaggerException('{} Failed to match album'.format(__name__))
+
+            if not fallback_taggers:
+                raise BeetsTaggerException('{} Failed to match album'.format(__name__))
+
+            for tagger in fallback_taggers:
+                fallback_recommendation = tagger.process(list_of_items)
+                artist_name, album_name, album_recommendation_list, recommendation = \
+                    tag_album(list_of_items, search_artist=expected_artist, search_album=expected_album,
+                              search_id=fallback_recommendation)
 
         distance, album_info, track_mapping, extra_items, extra_tracks = album_recommendation_list[0]
         logger.debug('Successfully matched album {} !'.format(album_info.album_id))

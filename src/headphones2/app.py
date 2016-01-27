05235e808e0f0221e250ad49800413ca666fbc6b
Omer Ben-Amram <omerbenamram@gmail.com>
2016-01-27 22:55:56 +0200

    cleaned up some trash

--- src/headphones2/app.py
+++ src/headphones2/app.py
@@ -14,21 +14,10 @@ from headphones2.views.api import ArtistList, ArtistResource, AlbumResource, Art
 
 FRONTEND_PATH = Path(__file__).parent.parent.joinpath("frontend")
 BUILD_PATH = FRONTEND_PATH.joinpath("dist")
-COMPONENTS_PATH = FRONTEND_PATH.joinpath("dist", "dev", "components")
-ASSETS_PATH = FRONTEND_PATH.joinpath("dist")
+ASSETS_PATH = FRONTEND_PATH.joinpath("dist", "assets")
 
-webpack_params = {
-    'DEBUG': True,
-    'WEBPACK_MANIFEST_PATH': '../frontend/dist/manifest.json',
-    'WEBPACK_ASSETS_URL': 'http://localhost:3000/assets/'
-}
-
-webpack = Webpack()
-
-# app = Flask('headphones2', instance_path=os.path.abspath(os.path.dirname(__file__)), static_folder=str(BUILD_PATH))
-app = Flask('headphones2', static_folder=str(BUILD_PATH), template_folder=str(FRONTEND_PATH.joinpath('app')))
+app = Flask('headphones2', static_folder=str(BUILD_PATH))
 app.debug = True
-app.config.update(webpack_params)
 
 logger = logbook.Logger('headphones2.app')
 
@@ -40,23 +29,12 @@ api.add_resource(AlbumResource, str('/album/<album_id>'), endpoint=str('album'))
 api.add_resource(Artwork, str('/artwork'), endpoint=str('artwork'))
 api.add_resource(ConfigurationResource, str('/configuration'), endpoint=str('configuration'))
 app_cache.init_app(app)
-webpack.init_app(app)
 
-
-# @app.route('/')
-# @app.route('/home')
-# def home():
-#     return render_template('index.jinja2')
 
 @app.route('/')
 @app.route('/home')
 def home():
     return send_from_directory(str(FRONTEND_PATH.joinpath('dist')), 'index.html')
-
-
-@app.route('/components/<path:path>')
-def serve_static(path):
-    return send_from_directory(str(COMPONENTS_PATH), path)
 
 
 @app.route('/assets/<path:path>')
@@ -67,11 +45,6 @@ def serve_asset(path):
 @app.route('/<path:path>')
 def serve_component(path):
     return send_from_directory(str(FRONTEND_PATH), path)
-
-
-@app.route('/app/bootstrap.js')
-def serve_bootstrap():
-    return send_file(str(BUILD_PATH.joinpath('bootstrap.js')))
 
 
 def main():

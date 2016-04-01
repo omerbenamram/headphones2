4bc87185367b4c5ba7dcda7d078c1772e683868d
Omer Ben-Amram <omerbenamram@gmail.com>
2016-04-01 15:00:21 +0300

    getting rid of unused code

--- src/headphones2/torrents/trackers.py
+++
@@ -1,8 +0,0 @@
-from torrentsearcher import KickAssTorrentsSearcher, PirateBaySearcher
-
-
-def get_configured_tracker_searchers():
-    return [
-        KickAssTorrentsSearcher(),
-        PirateBaySearcher()
-    ]
